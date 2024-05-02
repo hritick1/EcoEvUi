6a3e671d0ca28d92935a21663f805ae6d835f4e4
HRITICK KUMAR JHA <hjha9999@gmail.com>
2024-05-02 20:08:01 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ export default function Home({name}) {
     }
     const [person, setPerson] = useState({});
     const postData=()=>{
-      axios.post(`http://localhost1:8080/addDaily/${name}`,person).then((result) => {
+      axios.post(`http://localhost:8080/addDaily/${name}`,person).then((result) => {
         console.log(result);
         toast.success(result.data);
      }).catch((err) => {
